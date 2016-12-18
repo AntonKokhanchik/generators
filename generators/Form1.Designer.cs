90ef743a59ea71f1bbc03c65888b4f16a0e05180
Kokhanchik Anton <kokhanchik.tversu@gmail.com>
2016-12-18 13:21:02 +0300

    think that done

---
+++ generators/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace generators
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.buttonStart = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBoxKey = new System.Windows.Forms.TextBox();
+			this.textBoxSquare = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textBoxFib = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// buttonStart
+			// 
+			this.buttonStart.Location = new System.Drawing.Point(89, 51);
+			this.buttonStart.Name = "buttonStart";
+			this.buttonStart.Size = new System.Drawing.Size(100, 28);
+			this.buttonStart.TabIndex = 2;
+			this.buttonStart.Text = "Сгенерировать";
+			this.buttonStart.UseVisualStyleBackColor = true;
+			this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(94, 13);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Ключ генератора";
+			// 
+			// textBoxKey
+			// 
+			this.textBoxKey.Location = new System.Drawing.Point(15, 25);
+			this.textBoxKey.Name = "textBoxKey";
+			this.textBoxKey.Size = new System.Drawing.Size(100, 20);
+			this.textBoxKey.TabIndex = 1;
+			// 
+			// textBoxSquare
+			// 
+			this.textBoxSquare.Location = new System.Drawing.Point(12, 98);
+			this.textBoxSquare.Multiline = true;
+			this.textBoxSquare.Name = "textBoxSquare";
+			this.textBoxSquare.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.textBoxSquare.Size = new System.Drawing.Size(127, 194);
+			this.textBoxSquare.TabIndex = 3;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(12, 82);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(143, 13);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "Метод Середины квадрата";
+			// 
+			// textBoxFib
+			// 
+			this.textBoxFib.Location = new System.Drawing.Point(145, 98);
+			this.textBoxFib.Multiline = true;
+			this.textBoxFib.Name = "textBoxFib";
+			this.textBoxFib.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.textBoxFib.Size = new System.Drawing.Size(127, 194);
+			this.textBoxFib.TabIndex = 4;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(161, 82);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(99, 13);
+			this.label3.TabIndex = 1;
+			this.label3.Text = "Метод Фибоначчи";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(290, 304);
+			this.Controls.Add(this.textBoxFib);
+			this.Controls.Add(this.textBoxSquare);
+			this.Controls.Add(this.textBoxKey);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.buttonStart);
+			this.Name = "Form1";
+			this.Text = "Генерация случайных чисел";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button buttonStart;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox textBoxKey;
+		private System.Windows.Forms.TextBox textBoxSquare;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox textBoxFib;
+		private System.Windows.Forms.Label label3;
+	}
+}
+
---
+++ generators/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace generators
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void buttonStart_Click(object sender, EventArgs e)
+		{
+			squareMethod();
+			fibonacciMethod();
+		}
+
+		private void squareMethod()
+		{
+			textBoxSquare.Clear();
+			string number = textBoxKey.Text;
+
+			if (number.Length % 2 != 0)
+				number += "0";
+			int n = number.Length / 2;
+			number = "0," + number;
+
+			for (int i = 0; i < 17; i++)
+			{
+				double tmp = Double.Parse(number);
+				tmp *= tmp;
+
+				number = tmp.ToString();
+				while (number.Length < 4 * n + 2)
+					number += "0";
+				number = "0," + number.Substring(2 + n, 2 * n);
+
+				textBoxSquare.AppendText(number + "\n");
+			}
+		}
+
+		private void fibonacciMethod()
+		{
+			textBoxFib.Clear();
+			int a = 17 - 17, b = 17 - 5;
+			List<double> list = new List<double>();
+
+			foreach (string num in textBoxSquare.Text.Split('\n'))
+			{
+				double tmp;
+
+				if(Double.TryParse(num, out tmp))
+					list.Add(tmp);
+			}
+
+			for (int i = 0; i < 17; i++)
+			{
+				double newNum;
+
+				if (list[a] >= list[b])
+					newNum = list[a] - list[b];
+				else
+					newNum = list[a] - list[b] + 1;
+				list.RemoveAt(0);
+				list.Add(newNum);
+
+				textBoxFib.AppendText(newNum.ToString("f" + textBoxKey.TextLength) + "\n");
+			}
+		}
+	}
+}
